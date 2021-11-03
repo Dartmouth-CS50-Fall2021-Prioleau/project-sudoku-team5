cd2a9901c623841136620bb4363f8ff75ec4d1dd
Veronica R. Quidore <f004hbq@plank.thayer.dartmouth.edu>
2021-11-03 16:58:39 -0400

    pseudocode row/col helper

--- create/create.c
+++ create/create.c
@@ -22,6 +22,8 @@
 /************************  static helper  functions  prototypes ******************/
 /* that is, not visible outside this file */
 static void valueprint(FILE* fp, int value);
+static void sudoku_update_rows_cols_box(box_t* sudoku[9][9], int x, int y, int value, char* level);
+//static int get_rand_box_val(box_t* box, box_t* sudoku[9][9]);
 
 /***************************** global functions *********************************/
 /* that is, visible outside this file */
@@ -93,4 +95,17 @@ static void valueprint(FILE* fp, int value)
   }
 }
 
+static void sudoku_update_rows_cols_box(box_t* sudoku[9][9], int x, int y, int value, char* level)
+{
+    for (int i = 0; i < 9; i++) {
+        // set each key corresponding to the value in the relevant box counters to 0 within the row
+    }
+    for (int j = 0; j < 9; j++) {
+        // set each key corresponding to the value in the relevant box counters to 0 within the column
+    }
+    
+}
+// static int get_rand_box_val(box_t* box, box_t* sudoku[9][9])
+// {
 
+// }
