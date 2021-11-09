4911a1e8d5e0803f61f7ded35e302a7e5a5a8e59
Dylan R. Bienstock <f003wx1@plank.thayer.dartmouth.edu>
2021-11-09 16:16:18 -0500

    cehck for valid arguements/modes

--- sudoku.c
+++ sudoku.c
@@ -9,34 +9,65 @@
 #include "./library/memory.h"
 #include "./library/counters.h"
 #include "./library/file.h"
-#include "./solve/solve.h"
 #include "./puzzle/puzzle.h"
 #include "./common/build.h"
 #include "./common/unique.h"
 
-bool parse_user_sudoku(FILE* fp, puzzle_t* puzzle, char* level);
+bool parse_user_sudoku(FILE* fp, puzzle_t* puzzle);
 
 
 int main(const int argc, const char** argv)
 {
 
   //check for valid number of parameters
-  if (argc != 3) {
+  if (argc != 3 && argc != 2) {
 
     fprintf(stderr, "Incorrect number of arguments. ");
     fprintf(stderr,"Should have ./sudoku mode difficulty\n");
     return 1;
 
   }
-  
-  //allocate space for inputs and copy them to variables
-  char* mode = malloc(strlen(argv[1]) * sizeof(char) + 1);
-  char* difficulty = malloc(strlen(argv[2]) * sizeof(char) + 1);
 
-  
-  strcpy(mode, argv[1]);
-  strcpy(difficulty, argv[2]);
-  
+
+  char* mode;
+  char* difficulty; 
+
+  //Allocate space and copy to variable for inputs
+  if(argc == 2) {
+    mode = malloc(strlen(argv[1]) * sizeof(char) + 1);
+    strcpy(mode, argv[1]);
+
+  }
+  else {
+    mode = malloc(strlen(argv[1]) * sizeof(char) + 1);
+    difficulty = malloc(strlen(argv[2]) * sizeof(char) + 1);
+    strcpy(mode, argv[1]);
+    strcpy(difficulty, argv[2]);
+
+  }
+
+  //Check mode and difficulty are valid
+  if(strcmp(mode, "create") != 0 && strcmp(mode, "solve") != 0) {
+    fprintf(stderr, "Mode must be 'create' or 'solve'\n");
+    free(mode);
+    return 2;
+  }
+
+  if(argc == 3 ) {
+    if(strcmp(difficulty, "easy") != 0 && strcmp(difficulty, "hard") != 0) {
+    fprintf(stderr, "Difficulty must be 'easy' or 'hard'\n");
+    free(mode);
+    return 2;
+  }
+  }
+
+  //If 2 arguments are given make sure mode is solve
+  if(argc == 2 && strcmp(mode, "create") == 0 ) {
+    fprintf(stderr, "Create mode requires a third arguemnt for the difficulty\n");
+    free(mode);
+    return 3;
+  } 
+
   time_t t;
   srand((unsigned) time(&t));
 
@@ -47,7 +78,7 @@ int main(const int argc, const char** argv)
     puzzle_t* puzzle = puzzle_new(9);
 
     //build a fully filled sudoku
-    build_sudoku(puzzle, difficulty);
+    build_sudoku(puzzle, mode);
 
     //create a sudoku, removing points and checking for uniqueness
     //create_sudoku(puzzle);
@@ -65,9 +96,15 @@ int main(const int argc, const char** argv)
     FILE* file = fopen("parse.txt", "r");
 
     //take the input and build the puzzle struct from the data
-    parse_user_sudoku(file, parsed, difficulty);
+    parse_user_sudoku(file, parsed);
 
     puzzle_print_simple(parsed, stdout);
+
+    build_sudoku(parsed, mode);
+    
+    
+    puzzle_print_simple(parsed, stdout);
+
 
     /*
     bool is_solvable = solve_sudoku(parsed);
@@ -88,7 +125,7 @@ int main(const int argc, const char** argv)
 
 /**************************** bool parse_user_sudoku () **********************************/
 /* see sudoku.h for more detail */
-bool parse_user_sudoku(FILE* fp, puzzle_t* puzzle, char* level)
+bool parse_user_sudoku(FILE* fp, puzzle_t* puzzle)
 { 
   // return NULL if either fp or sudoku is NULL
   if (fp == NULL || puzzle == NULL ){
